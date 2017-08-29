3dd88ec3531d14d57c9f379388ba0b2854de1d18
Lindoélio Lázaro <lindoelio@gmail.com>
2017-08-28 21:38:09 -0300

    Add config for dependences versions.

--- package.js
+++ package.js
@@ -7,6 +7,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.5.1');
+
   api.use('ecmascript');
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
